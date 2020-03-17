b27c8cb06add7d26e1d5cb410ccb125e5dc8cc9f
Matt Kilcup <nevermindthelabel@gmail.com>
2020-03-16 21:43:01 -0700

    updated file with initial logic

--- src/views/todo-view.js
+++ src/views/todo-view.js
@@ -1,6 +1,27 @@
 import { LitElement, html } from 'lit-element';
 
+const VisibilityFilters = {
+  SHOW_ALL: 'All',
+  SHOW_ACTIVE: 'Active',
+  SHOW_COMPLETED: 'Completed'
+}
+
 class TodoView extends LitElement {
+
+  static get properties() {
+    return {
+      todos: { type: Array },
+      filter: { type: String },
+      task: { type: String }
+    }
+  }
+
+  constructor() {
+    super();
+    this.todos = [];
+    this.filter = VisibilityFilters.SHOW_ALL;
+    this.task = '';
+  }
 
   render() {
     return html`
