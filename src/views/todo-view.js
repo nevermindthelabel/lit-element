f34cd61ea7f018c5392e1af2698336e51637f621
Matt Kilcup <nevermindthelabel@gmail.com>
2020-03-16 21:53:52 -0700

    updated render function

--- src/views/todo-view.js
+++ src/views/todo-view.js
@@ -1,4 +1,9 @@
 import { LitElement, html } from 'lit-element';
+import '@vaadin/vaadin-text-field';
+import '@vaadin/vaadin-checkbox';
+import '@vaadin/vaadin-radio-button/vaadin-radio-button';
+import '@vaadin/vaadin-radio-button/vaadin-radio-group';
+import '@vaadin/vaadin-button';
 
 const VisibilityFilters = {
   SHOW_ALL: 'All',
@@ -25,8 +30,32 @@ class TodoView extends LitElement {
 
   render() {
     return html`
-      <p>Hello World</p>
+      <div class="input-layout">
+      <vaadin-text-field
+      placeholder="Task"
+      value="${this.task}"
+      @change="${this.updateTask}"
+      ></vaadin-text-field>
+      <vaadin-button
+      theme="primary"
+      @click="${this.addTodo}"
+      >Add Todo</vaadin-button>
+      </div>
     `
+  }
+
+  updateTask(e) {
+    this.task = e.target.value;
+  };
+
+  addTodo() {
+    if (this.task) {
+      this.todos = [...this.todos, {
+        task: this.task,
+        complete: false
+      }];
+      this.task = '';
+    }
   }
 }
 
